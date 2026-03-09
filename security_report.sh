8824e1247b7c92eb306fdfd69b9be97699c20fcb
StefanPpica <stefan.popicaa@gmail.com>
2026-03-09 18:12:38 +0200

    generate logs + security check

---
+++ security_report.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+LOG_FILE="auth.log"
+
+echo "=== RAPORT DE SECURITATE - $(date) ==="
+echo "--------------------------------------"
+
+# 1. Identigicam IP-ul cu cele mai multe esecuri
+MOST_ACTIVE_IP=$(grep "Failed" $LOG_FILE | awk '{print $11}' | sort | uniq -c |
+
+echo "Atacatorul principal (incercari | IP):"
+echo "$MOST_ACTIVE_IP"
+echo "-------------------------------------"
+
+# 2. Listam utilizatorii care trebuie sa isi schimbe parola(cei care au Accepted)
+echo "Logari reusite (Audit):"
+grep "Accepted" $LOG_FILE | awk '{print $9 " de la IP-ul " $11}'
